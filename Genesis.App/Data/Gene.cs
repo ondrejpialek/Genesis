a2b76f85962e8a5e66dc5ca9416dee499ada92e4
Ondřej Piálek <ondrej@pialek.eu>
2015-09-06 23:52:40 +0200

    2014 localdb removed empty migration splitting gene

--- Genesis.App/Data/Gene.cs
+++ Genesis.App/Data/Gene.cs
@@ -10,23 +10,53 @@ namespace Genesis
     public class Gene
     {
         public virtual int Id { get; private set; }
+
         [Required]
         public virtual string Name { get; set; }
+
         [Required]
         public virtual Chromosome Chromosome { get; set; }
 
         public virtual int StartBasePair { get; set; }
 
-        public virtual ICollection<Allele> Alleles { get; private set; }
+        public Gene(int id)
+        {
+            Id = id;
+        }
 
         public Gene()
+        {
+
+        }
+    }
+
+    public class NominalGene : Gene
+    {
+        public virtual ICollection<Allele> Alleles { get; private set; }
+
+        public NominalGene()
         {
             Alleles = new List<Allele>();
         }
 
-        public Gene(int id) : this() 
+        public NominalGene(int id) : base(id)
         {
-            Id = id;
+            Alleles = new List<Allele>();
+        }
+    }
+
+    public class OrdinalGene : Gene
+    {
+        public virtual double Min { get; set; }
+        public virtual double Max { get; set; }
+
+        public OrdinalGene(int id) : base(id)
+        {
+
+        }
+        public OrdinalGene()
+        {
+
         }
     }
 }
